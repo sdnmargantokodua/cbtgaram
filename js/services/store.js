6b285b88ab16f5d5f92bd52a5ffec0e4491c4852
sdnmargantokodua <sdnmargantokodua@gmail.com>
2026-04-30 15:20:44 +0700

    Create store.js

---
+++ js/services/store.js
@@ -0,0 +1,11 @@
+// js/services/store.js
+export const state = {
+    allResults: [],
+    allQuestions: [],
+    examSchedules: [],
+    examParticipants: [],
+    schoolProfile: {},
+    currentSubject: "Pendidikan Pancasila",
+    allSubjectsSet: new Set(["Pendidikan Pancasila", "Matematika", "Bahasa Indonesia", "IPA", "IPS"]),
+    ADMIN_PIN: "ADMIN2026"
+};
