91b98bc2f92b7044a0d45580f5f16a5c72ddcf55
sdnmargantokodua <sdnmargantokodua@gmail.com>
2026-04-30 15:21:50 +0700

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// js/main.js
+import './config/firebase.js';
+import './services/store.js';
+import './services/api.js';
+import './ui/adminUI.js';
+import './ui/print.js';
+
+console.log("Sistem Admin CBT SAKERA Terinisialisasi.");
